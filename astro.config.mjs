2c596bd60c37bffb6eafdd7e4d2e105af652ff86
Nico <nico@varrix.dev>
2023-04-24 17:05:43 -0500

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://nico-himself.github.io',
+	// base: '/my-repo',
 	integrations: [mdx(), tailwind()],
 });
