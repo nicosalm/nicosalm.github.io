4124a9231c8dbe08ecac0c76bec754aea2bd8b95
Nico Salm <nico@salm.dev>
2023-04-27 11:37:22 -0500

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Nico Salm';
+export const SITE_DESCRIPTION = 'My personal website and blog - Nico Salm.';
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,27 @@
+/*@type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require('@tailwindcss/typography'), require('daisyui')],
+	daisyui: {
+		themes: [
+			{
+				emerald: {
+					...require('daisyui/src/colors/themes')['[data-theme=emerald]'],
+					'.sidebar': {
+						'background-color': '#F9FAFB',
+					},
+				},
+
+				forest: {
+					...require('daisyui/src/colors/themes')['[data-theme=business]'],
+					'.sidebar': {
+						'background-color': 'hsl(var(--b2))',
+					},
+				},
+			},
+		],
+	},
 };
